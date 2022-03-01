24d3512c7caa5e6b60db2ff40d55e37224c42766
Adam Nemeth <nemethadam88@gmail.com>
2022-03-01 16:11:12 +0100

    first steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Pagination = styled.div`
+  display: flex;
+`;
+
+const PageButton = styled.div`
+  background-color: #fffaaa;
+  padding: 4px;
+  margin: 4px;
+`;
 
 function App() {
+  const limit = 50;
+  const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(0);
+  const [offset, setOffset] = useState(0);
+
+  const baseURL = `https://pokeapi.co/api/v2`;
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/pokemon`, {
+        params: {
+          offset,
+          limit,
+        },
+      })
+      .then((res) => {
+        setPage(res.data.count / limit + 1);
+        setPokemons(res.data.results);
+        console.log(res.data);
+      });
+    console.log("hello");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {pokemons.map((pokemon) => (
+          <div>{pokemon.name}</div>
+        ))}
+      </div>
+
+      <Pagination>
+        {Array.from({ length: page }, (_, k) => (
+          <PageButton key={k}>{k}</PageButton>
+        ))}
+      </Pagination>
     </div>
   );
 }
